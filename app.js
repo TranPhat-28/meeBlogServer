4031c0e6c2489a9bc563d4591ced14c7f3374dfe
TranPhat-28 <62002249+TranPhat-28@users.noreply.github.com>
2022-11-07 09:18:43 -0500

    Add file

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const Blog = require('./models/Blog')
+require('dotenv').config()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+// Connect to MongoDB
+// MongoDB connection
+const db = process.env.DATABASE_URL;
+mongoose.connect(db, {useUnifiedTopology: true, useNewUrlParser: true})
+.then(() => console.log('Connected to MongoDB'))
+.catch(e => console.log(e.message));
+
+
+// Fetch all blogs
+app.get('/blogs', (req, res) => {
+    Blog.find().then(data => {
+        res.json(data)
+    })
+})
+
+// POST to create new blog
+app.post('/blogs', (req, res) => {
+    const newBlog = new Blog({
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content
+    })
+
+    newBlog.save().then(() => {
+        res.sendStatus(200)
+    })
+    .catch(e => {
+        console.log(e)
+        res.sendStatus(500)
+    })
+})
+
+// POST to add comment
+app.post('/blogs/:id', (req, res) => {
+    Blog.findOneAndUpdate({ _id: req.params.id }, { $push: { comment: req.body.comment } })
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(e => {
+        console.log(e)
+        res.sendStatus(500)
+    })
+})
+
+// Fetch blog's comment
+app.get('/comments/:id/:page', (req, res) => {
+    Blog.findOne({ _id: req.params.id })
+    .then((blog) => {
+        const arr = blog.comment
+        const resArr = arr.slice((req.params.page - 1) * 3, (req.params.page - 1) * 3 + 3)
+        res.json(resArr)
+    })
+    .catch(e => {
+        console.log(e)
+        res.sendStatus(500)
+    })
+})
+
+// GET blogs content
+app.get('/view/:id', (req, res) => {
+    Blog.findOne({_id: req.params.id}).then((blog) => {
+        //console.log(blog)
+        res.json(blog)
+    })
+    .catch(e => {
+        console.log(e.message)
+        res.sendStatus(500)
+    })
+})
+
+// Delete blogs
+app.delete('/blogs/:id', (req, res) => {
+    Blog.deleteOne({ _id: req.params.id}).then(() => {
+        res.sendStatus(200)
+    })
+    .catch(e => { 
+        console.log(e.message)
+        res.sendStatus(500)
+    })
+})
+
+const PORT = process.env.port || 5000
+
+app.listen(PORT, () => {
+    console.log(`App started on port ${PORT}`)
+})
---
+++ models/Blog.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+// Define schema
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+        require: true
+    },
+    content: {
+        type: String,
+        require: true
+    },
+    datePosted: {
+        type: Date,
+        default: Date.now()
+    },
+    comment: {
+        type: [ String ],
+    }
+})
+
+const Blog = mongoose.model('Blog', BlogSchema);
+module.exports = Blog;
