5eb4d7a95ac262ffa593d259b0703cd87b50f0e1
TranPhat-28 <62002249+TranPhat-28@users.noreply.github.com>
2022-11-07 23:06:53 +0700

    Update app.js
    Fix typo: process.env.PORT

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.delete('/blogs/:id', (req, res) => {
     })
 })
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`App started on port ${PORT}`)
